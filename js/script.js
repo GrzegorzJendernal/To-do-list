3f0b71a005fdf63c2b237bb947a5c8af91d8030d
Grzegorz Jendernal <g.jendernal@gmail.com>
2022-09-12 16:39:00 +0200

    added immutability

--- js/script.js
+++ js/script.js
@@ -3,42 +3,52 @@
         console.log("Hej! Życzę miłego sprawdzania mojej pracy domowej. :)");
     };
 
-    const tasks = [];
+    let tasks = [];
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks = [
+            ...tasks,
+            { content: newTaskContent },
+        ];
 
         render();
     };
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
 
         render();
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            { ...tasks[taskIndex], done: !tasks[taskIndex].done },
+            ...tasks.slice(taskIndex + 1),
+        ];
 
         render();
-    }
+    };
 
-    const bindEvents = () => {
-        const removeButtons = document.querySelectorAll(".js-taskRemove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index);
-            });
-        });
-
+    const bindDoneEvents = () => {
         const toggleDoneButtons = document.querySelectorAll(".js-taskDone");
 
         toggleDoneButtons.forEach((toggleDoneButton, index) => {
             toggleDoneButton.addEventListener("click", () => {
                 toggleTaskDone(index);
+            });
+        });
+    };
+
+    const bindRemoveEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-taskRemove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
             });
         });
     }
@@ -48,16 +58,17 @@
 
         for (const task of tasks) {
             htmlString += `
-    <li class="list__task">
-    <button class="list__button js-taskDone">${task.done ? "✔" : ""}</button>
-    <span${task.done ? " class=\"list__task--done\"" : ""}>${task.content}</span>
-    <button class="list__button list__button--remove js-taskRemove">🗑</button>
-    </li>`;
+            <li class="list__task">
+                <button class="list__button js-taskDone">${task.done ? "✔" : ""}</button>
+                <span${task.done ? " class=\"list__task--done\"" : ""}>${task.content}</span>
+                <button class="list__button list__button--remove js-taskRemove">🗑</button>
+            </li>`;
         };
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-        bindEvents();
+        bindRemoveEvents();
+        bindDoneEvents();
     };
 
     const resetInput = (inputElement) => {
